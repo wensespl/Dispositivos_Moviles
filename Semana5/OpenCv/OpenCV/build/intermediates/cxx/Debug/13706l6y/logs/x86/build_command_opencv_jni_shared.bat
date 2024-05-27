8772274fa0e349ec91aa3e8392110766556dfc40
Wenses Johan Penadillo Lazares <wenses400@gmail.com>
2024-05-27 14:16:49 -0500

    Delete Semana5/OpenCv/OpenCV directory

--- Semana5/OpenCv/OpenCV/build/intermediates/cxx/Debug/13706l6y/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\USUARIO\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\USUARIO\\Desktop\\Dispositivos_Moviles\\Semana5\\OpenCv\\OpenCV\\.cxx\\Debug\\13706l6y\\x86" ^
-  opencv_jni_shared
