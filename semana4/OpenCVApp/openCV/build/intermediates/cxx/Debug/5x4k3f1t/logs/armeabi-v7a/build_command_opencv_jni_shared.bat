7cab563b5f2bdee63b31fbd2fde24a3823e099de
Wenses Johan Penadillo Lazares <wenses400@gmail.com>
2024-12-17 10:29:19 -0500

    Delete semana4/OpenCVApp/openCV directory

--- semana4/OpenCVApp/openCV/build/intermediates/cxx/Debug/5x4k3f1t/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\USUARIO\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\USUARIO\\Desktop\\Dispositivos_Moviles\\semana4\\OpenCVApp\\openCV\\.cxx\\Debug\\5x4k3f1t\\armeabi-v7a" ^
-  opencv_jni_shared
